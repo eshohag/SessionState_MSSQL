f4e74531542044ca6ebf760ffa0e4270239e9145
eshohag <eshohag@outlook.com>
2024-01-31 20:02:29 +0600

    Add project files.

---
+++ SessionState_MSSQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SessionState_MSSQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SessionState_MSSQL.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SessionState_MSSQL.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ SessionState_MSSQL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SessionState_MSSQL.Startup))]
+namespace SessionState_MSSQL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
